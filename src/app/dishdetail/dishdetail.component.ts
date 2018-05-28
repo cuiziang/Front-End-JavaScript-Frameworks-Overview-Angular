7a3a26b9f8bea5b129fb4f74b3149026345f1152
Ziang Cui <mrchoisss@gmail.com>
2018-05-28 18:46:09 -0400

    Data Binding

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,46 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-
-const DISH = {
-  name: 'Uthappizza',
-  image: '/assets/images/uthappizza.png',
-  category: 'mains',
-  label: 'Hot',
-  price: '4.99',
-  description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe ' +
-  'vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-  comments: [
-    {
-      rating: 5,
-      comment: 'Imagine all the eatables, living in conFusion!',
-      author: 'John Lemon',
-      date: '2012-10-16T17:57:28.556094Z'
-    },
-    {
-      rating: 4,
-      comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-      author: 'Paul McVites',
-      date: '2014-09-05T17:57:28.556094Z'
-    },
-    {
-      rating: 3,
-      comment: 'Eat it, just eat it!',
-      author: 'Michael Jaikishan',
-      date: '2015-02-13T17:57:28.556094Z'
-    },
-    {
-      rating: 4,
-      comment: 'Ultimate, Reaching for the stars!',
-      author: 'Ringo Starry',
-      date: '2013-12-02T17:57:28.556094Z'
-    },
-    {
-      rating: 2,
-      comment: 'It\'s your birthday, we\'re gonna party!',
-      author: '25 Cent',
-      date: '2011-12-02T17:57:28.556094Z'
-    }
-  ]
-};
+import {Component, OnInit, Input} from '@angular/core';
+import {Dish} from '../shared/dish';
 
 @Component({
   selector: 'app-dishdetail',
@@ -49,9 +8,8 @@ const DISH = {
 })
 export class DishdetailComponent implements OnInit {
 
-  dish = DISH;
-
-  comments = DISH['comments'];
+  @Input()
+  dish: Dish;
 
   constructor() {
   }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,41 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {Dish} from '../shared/dish';
-
-const DISHES: Dish[] = [
-  {
-    name: 'Uthappizza',
-    image: '/assets/images/uthappizza.png',
-    category: 'mains',
-    label: 'Hot',
-    price: '4.99',
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola ' +
-    'olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-  },
-  {
-    name: 'Zucchipakoda',
-    image: '/assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    label: '',
-    price: '1.99',
-    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-  },
-  {
-    name: 'Vadonut',
-    image: '/assets/images/vadonut.png',
-    category: 'appetizer',
-    label: 'New',
-    price: '1.99',
-    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-  },
-  {
-    name: 'ElaiCheese Cake',
-    image: '/assets/images/elaicheesecake.png',
-    category: 'dessert',
-    label: '',
-    price: '2.99',
-    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-  }
-];
+import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
 
 @Component({
   selector: 'app-menu',
@@ -46,11 +11,17 @@ export class MenuComponent implements OnInit {
 
   dishes = DISHES;
 
+  selectedDish: Dish;
+
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onSelect(dish: Dish) {
+    this.selectedDish = dish;
   }
 
 }
