bfc842c631750d6ff9432a6fb3fccf494f0de3f0
Ziang Cui <mrchoisss@gmail.com>
2018-05-30 13:30:20 -0400

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,12 +17,16 @@ import {MenuComponent} from './menu/menu.component';
 import {DishdetailComponent} from './dishdetail/dishdetail.component';
 
 import {DishService} from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
